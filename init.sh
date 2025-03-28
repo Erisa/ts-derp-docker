2ca3c7c913ce7c14721a1e571d78628d9aa9643a
Erisa A <erisa@tailscale.com>
2025-03-28 10:53:04 +0000

    Add TS_DERP_EXTRA_ARGS
    Fixes #3

--- init.sh
+++ init.sh
@@ -6,4 +6,4 @@ if [ "$TS_RUN_TAILSCALE" = true ] ; then
 fi
 
 # Start the derp server
-/usr/local/bin/derper --hostname $TS_DERP_HOSTNAME --bootstrap-dns-names $TS_DERP_HOSTNAME -certmode $TS_DERP_CERTMODE -certdir /root/derper/$TS_DERP_HOSTNAME --stun --verify-clients=$TS_DERP_VERIFY_CLIENTS --http-port=$TS_DERP_HTTP_PORT -a :$TS_DERP_LISTEN_PORT --stun-port=$TS_DERP_STUN_PORT
+/usr/local/bin/derper --hostname $TS_DERP_HOSTNAME --bootstrap-dns-names $TS_DERP_HOSTNAME -certmode $TS_DERP_CERTMODE -certdir /root/derper/$TS_DERP_HOSTNAME --stun --verify-clients=$TS_DERP_VERIFY_CLIENTS --http-port=$TS_DERP_HTTP_PORT -a :$TS_DERP_LISTEN_PORT --stun-port=$TS_DERP_STUN_PORT $TS_DERP_EXTRA_ARGS
